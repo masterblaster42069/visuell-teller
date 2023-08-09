7eaea70e8e70bb0eeb477bb55cf51bf556ef4af2
masterblaster42069 <114529314+masterblaster42069@users.noreply.github.com>
2023-08-09 14:59:46 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var counter=0;
+
+function counterplus(){
+    counter++;
+    document.getElementById("counter").innerHTML=counter;
+}
+
+function counterminus(){
+    counter--;
+    document.getElementById("counter").innerHTML=counter;
+}
